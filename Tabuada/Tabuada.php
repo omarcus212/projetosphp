956999ba0a7c5844089f05dcc49940a840d44a18
omarcus212 <marcusvinniciusouza@gmail.com>
2022-02-07 21:56:23 -0300

    ja tem a Tabuada

---
+++ Tabuada/Tabuada.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="cssTabuada.css">
+    <title>Tabuada</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="menu-hambu">
+            <input type="checkbox" name="menu-burg" id="menuburg">
+            <label for="menuburg">
+                <div class="menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </label>
+            <div class="menuzinho">
+                <div>
+                    <div>
+                        <ul>
+                            <li><a href="../Calculadora/calculadora_simples.php">calculadora</a></li>
+                            <li><a href="../Media/media.php">Media</a></li>
+                            <li><a href="">Tabuada</a></li>
+                            <li><a href="">Impar e Par</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <main>
+
+        <div id="conteudo">
+            <div id="titulo">
+                <h4>Tabuada </h4>
+            </div>
+
+            <div id="form">
+                <form name="frmcalculadora" method="post" action="">
+
+                    Multiplicador: <input type="text" name="txtn1" value="0" id="multcaixa"> <br> Maxmultiplicador: <input type="text" name="txtn2" value="0" id="maxcaixa"> <br>
+                    <div id="container_opcoes">
+
+                        <input type="submit" name="btncalc" value="Calcular" id="calc">
+                        <input type="submit" name="btnlimpar" value="limpar" id="limpar">
+
+                    </div>
+                    <div id="resultado">
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
